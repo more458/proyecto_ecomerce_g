dbfd50f77c462181cfdb54ff9c38028611ebb4f9
more458 <tomimore521@gmail.com>
2025-04-21 18:31:38 -0300

    Añadiendo archivos nuevos

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/us', 'Home::quienesSomos');
+
--- app/Views/front/nav_view.php
+++ app/Views/front/nav_view.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="app/Views/front/quienesSomos.php">Quienes Somos</a>
+          <a class="nav-link" href="app/Views/front/quienesSomos">Quienes Somos</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Comercializacion</a>
