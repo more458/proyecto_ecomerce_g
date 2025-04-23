365e5e8dd4fbd99ffd155ffa90c85e8ac7ff7940
jGalie <juli.gauna09@gmail.com>
2025-04-23 20:57:16 -0300

    Actualización Info_Contact

---
+++ app/Views/front/Info_Contact.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "assets/css/bootstrap.min.css">
+    <title></title>
+</head>
+<body>
+<div class="row g-3">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+  </div>
+</div>
+
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
